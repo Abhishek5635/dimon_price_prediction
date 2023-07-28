94c5889d2dddd3e17193fdac5d2793436d7f04a6
Abhishek5635 <abhishekwankhade4670@gmail.com>
2023-07-28 11:57:42 +0530

    new dep

--- application.py
+++ application.py
@@ -34,5 +34,5 @@ def predict_datapoint():
 
         return render_template('Results.html', final_result = result)
     
-if __name__ == ("__main__"):
-    app.run(host= "0.0.0.0", debug = True)
+if __name__ == "__main__":
+    app.run(host= "0.0.0.0")
