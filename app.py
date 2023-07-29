03babef1cb16d708a7fbc00e256c7bec99aa71d2
Abhishek5635 <abhishekwankhade4670@gmail.com>
2023-07-29 19:11:39 +0530

    Trigger the app runner

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
 application = Flask(__name__)
+
 app = application
+
 
 @app.route('/')
 def hompage():
